228ee826e74f73a0a6ea84ef313ae093851aef53
Mosa <Mohamedmosa6000@gmail.com>
2026-02-01 23:55:20 +0200

    Add Entities in Core Layer

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureURL { get; set; }
+        //[ForeignKey("ProductBrand")] We Use fluent api 
+        public int ProductBrandId { get; set; }
+        public ProductBrand ProductBrand { get; set; } // Navigation Property [1->M]
+
+       //[ForeignKey("ProductType")] We Use fluent api 
+        public int ProductTypeId { get; set; }
+        public ProductType ProductType { get; set; } // Navigation Property [1->M]
+
+    }
+}
