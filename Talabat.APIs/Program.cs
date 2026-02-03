4af974cd4a347e08f4b6fa47fbab8ed53e50cdbc
Mosa <Mohamedmosa6000@gmail.com>
2026-02-03 02:38:04 +0200

    Add Data Seed

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -1,19 +1,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Talabat.Repository.Data;
 
 namespace Talabat.APIs
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+           using var Scope = host.Services.CreateScope();
+            var Services = Scope.ServiceProvider;
+            var LoggerFactory = Services.GetService<ILoggerFactory>();
+            try
+            {
+
+                var Context = Services.GetRequiredService<StoreContext>();
+                await Context.Database.MigrateAsync(); // Update-Database 
+               await StoreContextSeed.SeedAsync(Context, LoggerFactory);
+            }
+            catch (Exception ex)
+            {
+                var logger = LoggerFactory.CreateLogger<Program>();
+                logger.LogError(ex,ex.Message);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Talabat.Repository/Data/StoreContextSeed.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+
+
+namespace Talabat.Repository.Data
+{
+    public class StoreContextSeed
+    {
+
+        public static async Task SeedAsync(StoreContext context , ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.productBrands.Any())
+                {
+                    var brandsData = File.ReadAllText("../Talabat.Repository/Data/DataSeed/brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                    foreach (var brand in brands)
+                        context.Set<ProductBrand>().Add(brand);
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.productTypes.Any())
+                {
+                    var typesData = File.ReadAllText("../Talabat.Repository/Data/DataSeed/types.json");
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                    foreach (var type in types)
+                        context.Set<ProductType>().Add(type);
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.products.Any())
+                {
+                    var productsData = File.ReadAllText("../Talabat.Repository/Data/DataSeed/products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                    context.products.AddRange(products);
+                    await context.SaveChangesAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex, ex.Message);
+
+            }
+
+
+
+            
+        }
+
+    }
+}
